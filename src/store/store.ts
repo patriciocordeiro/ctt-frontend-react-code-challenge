764767c33fa2567989b39b6c8892e5f81e0bd27f
patriciocordeiro <paponcio6@gmail.com>
2025-06-08 18:53:37 +0100

    Added redux-thunk middleware and configure store for async actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,12 @@
-import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, undefined, composedEnhancer);
+
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
