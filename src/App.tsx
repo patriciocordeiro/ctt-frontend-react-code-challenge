9d72836fefddc474aba74589c8a4e57dd7aad684
patriciocordeiro <paponcio6@gmail.com>
2025-06-08 18:20:01 +0100

    Setup rootReducer, Redux store, and provide to React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { ProductList } from './app/product/ProductList';
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <h1> CTT Exercise - Frontend Typescript!</h1>
+      <ProductList />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/store';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -8,7 +10,9 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
