233548f5e64cd8e759e914911b1ee8b76c297a51
patriciocordeiro <paponcio6@gmail.com>
2025-06-08 19:50:43 +0100

    Ensure correct thunk middleware application and improve dispatch typing

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,24 @@
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import rootReducer from './rootReducer';
+import { thunk, ThunkAction, ThunkDispatch } from 'redux-thunk';
+import { ProductAction } from './product/product.types';
+import rootReducer, { RootState } from './rootReducer';
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
+export type AppAction = ProductAction;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AppAction
+>;
+
+export type AppDispatch = ThunkDispatch<RootState, undefined, AppAction>;
+
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware<AppDispatch, RootState>(thunk)
+);
 
 const store = createStore(rootReducer, undefined, composedEnhancer);
-
-export type AppDispatch = typeof store.dispatch;
 
 export default store;
