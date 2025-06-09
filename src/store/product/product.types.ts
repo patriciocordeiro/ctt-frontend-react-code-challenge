9e9e83dbe4c31b983905475c8ac7656037ecf35f
patriciocordeiro <paponcio6@gmail.com>
2025-06-09 17:31:07 +0100

    Define action types for updating products

--- src/store/product/product.types.ts
+++ src/store/product/product.types.ts
@@ -8,6 +8,10 @@ export const CREATE_PRODUCT_REQUEST = 'CREATE_PRODUCT_REQUEST';
 export const CREATE_PRODUCT_SUCCESS = 'CREATE_PRODUCT_SUCCESS';
 export const CREATE_PRODUCT_FAILURE = 'CREATE_PRODUCT_FAILURE';
 
+export const UPDATE_PRODUCT_REQUEST = 'UPDATE_PRODUCT_REQUEST';
+export const UPDATE_PRODUCT_SUCCESS = 'UPDATE_PRODUCT_SUCCESS';
+export const UPDATE_PRODUCT_FAILURE = 'UPDATE_PRODUCT_FAILURE';
+
 interface FetchProductsRequestAction {
   type: typeof FETCH_PRODUCTS_REQUEST;
 }
@@ -36,13 +40,30 @@ export interface CreateProductFailureAction {
   payload: string;
 }
 
+export interface UpdateProductRequestAction {
+  type: typeof UPDATE_PRODUCT_REQUEST;
+}
+
+export interface UpdateProductSuccessAction {
+  type: typeof UPDATE_PRODUCT_SUCCESS;
+  payload: Product;
+}
+
+export interface UpdateProductFailureAction {
+  type: typeof UPDATE_PRODUCT_FAILURE;
+  payload: string;
+}
+
 export type ProductAction =
   | FetchProductsRequestAction
   | FetchProductsSuccessAction
   | FetchProductsFailureAction
   | CreateProductRequestAction
   | CreateProductSuccessAction
-  | CreateProductFailureAction;
+  | CreateProductFailureAction
+  | UpdateProductRequestAction
+  | UpdateProductSuccessAction
+  | UpdateProductFailureAction;
 
 export interface ProductState {
   readonly items: Product[];
